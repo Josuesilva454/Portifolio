d63a5d74e86f18ce14df4c98203408499e5e31d4
Josué Silva <josuesilvarosa20@hotmail.com>
2023-07-10 23:55:21 -0300

    Meu projeto

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import desafio from "../img/imagem.png";
+import "../styles/NavBar.css";
+
 
 const Projects = () => {
   return (
     <div>
-      projetos
+      <h1>Projeto mais desafiadores</h1>
+      <a>Rastreabilidade da origem do minério</a> 
+      <div className='dev-img'><img src={desafio}></img></div>
+   
     </div>
   )
 }
